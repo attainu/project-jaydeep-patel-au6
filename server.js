31252af50ceab578e3439b412b9483d69e6a0399
jaydeep patel <jppatel147@gmail.com>
2020-09-22 11:54:42 +0530

    BE - Account active link sent thrue mail, user model, route, controller, validation

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const connectionDB = require('./config/db.js')
+
+const app = express()
+
+
+
+//config .env
+require('dotenv').config({
+    path : './config/config.env'
+})
+
+//body parsers
+app.use(bodyParser.json())
+
+//connection to db
+connectionDB()
+
+
+//config for only development
+if(process.env.NODE_ENV === 'development'){
+    app.use(cors({
+        origin: process.env.CLIENT_URL
+    }))
+
+    //morgan give info about each request
+    //cors deal with react
+    app.use(morgan('dev'))
+}
+
+
+//load all routes
+const authRoute = require('./routes/authRoute.js')
+
+
+
+
+//use route
+app.use('/api/', authRoute)
+
+
+
+
+
+
+app.use((req, res)=>{
+    res.status(404).json({
+        sucess: false,
+        msg: 'Page not found'
+    })
+})
+
+
+const PORT = process.env.PORT
+
+app.listen(PORT, ()=>{
+    console.log(`App lisning on port ${PORT}`)
+})
