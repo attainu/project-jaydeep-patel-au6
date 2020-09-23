b6f9fee6477c7c0c758ced4163c0711cc02bd29c
jaydeep patel <jppatel147@gmail.com>
2020-09-23 17:39:47 +0530

    password reset BE FE

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import Register from './Screens/Register.jsx'
 import Login from './Screens/Login.jsx'
 import Activate from './Screens/Activate'
 import ForgetPassword from './Screens/ForgetPassword';
+import ResetPassword from './Screens/ResetPassword'
 
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -19,6 +20,7 @@ ReactDOM.render(
       <Route path="/login" exact render={props => <Login {...props} />} /> 
       <Route path='/users/activate/:token' exact render={props => <Activate {...props} />} />
       <Route path='/users/password/forget' exact render={props => <ForgetPassword {...props} />} />
+      <Route path='/users/password/reset/:token' exact render={props => <ResetPassword {...props} />} />
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -7,7 +7,8 @@ const {
     registerController,
     activationController,
     signinController,
-    forgotPasswordController
+    forgotPasswordController,
+    resetPasswordController
 } = require('../controllers/authController.js')
 
 
@@ -23,5 +24,6 @@ const {
 router.post('/register' ,validRegister, registerController)
 router.post('/login' ,validLogin, signinController)
 router.post('/activation', activationController)
-router.put('/forgotpassword', forgotPasswordValidator, forgotPasswordController);
+router.put('/forgotpassword', forgotPasswordValidator, forgotPasswordController)
+router.put('/resetpassword', resetPasswordValidator, resetPasswordController);
 module.exports = router
